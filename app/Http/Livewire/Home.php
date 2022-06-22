a5ecdc21096c373a8a33e014f80f921e9d033512
Yoel Baez <jbaezgis@gmail.com>
2022-06-22 11:23:43 -0400

    Design - 1.0.02

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -6,8 +6,21 @@ use Livewire\Component;
 
 class Home extends Component
 {
+
+    public $modal = false;
+
     public function render()
     {
         return view('livewire.home')->layout('layouts.guest');
+    }
+
+    public function openModal()
+    {
+        $this->modal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->modal = false;
     }
 }
